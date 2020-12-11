f5ebe7f7ab313e644418f64e6882ff9f02f65263
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2020-12-10 23:45:13 -0500

    añadido bluprints oara la UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import { Icon, Intent } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import './App.css';
 
 function App() : JSX.Element {
   return (
     <>
-      
+      <Icon icon="cross" />
+      <Icon icon="cross" iconSize={20} />
+ 
+
+      <Icon icon={IconNames.CROSS} iconSize={Icon.SIZE_LARGE} intent={Intent.PRIMARY} color="red" />
     </>
   );
 }
