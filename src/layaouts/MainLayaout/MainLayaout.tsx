a973be8b4a4fe82f4cb62d6f8a9f344284086840
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2020-12-28 07:35:39 -0500

    empezando a maquetar Main Layaout

--- src/layaouts/MainLayaout/MainLayaout.tsx
+++ src/layaouts/MainLayaout/MainLayaout.tsx
@@ -1,26 +1,27 @@
 import React, { useState, FunctionComponent } from 'react';
 import { Route, Switch  } from 'react-router-dom';
-import { Switch as BluePrintSwitch } from '@blueprintjs/core';
-
-import HomePage from '../../pages/Home/Home';
-import Login from '../../pages/Login/Login';
+import { Navbar, Alignment, Button } from '@blueprintjs/core';
 //interfaces
 import { Ilayaout, Iroute, layaoutProps } from '../../interfaces/routes';
 
+import './MainLayaout.scss'
 
 const MainLayaout: React.FC<layaoutProps> = ({ routes }) => {
-  console.log("Routes : ", routes);
   return ( 
     <div>
-      <h2>Header ...</h2>
-
-        <Route
-          path="/login"
-          exact = {true}
-        >
-          <Login></Login>
-        </Route> 
-
+      <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>PWEEL</Navbar.Heading>
+            <Navbar.Divider />
+        </Navbar.Group>
+        <Navbar.Group align = { Alignment.RIGHT } >
+            <Button className="bp3-minimal" icon="home" text="Home" />
+            <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
+      </Navbar>
+      <LoadRoutes
+        routes = { routes }
+      />
       <h2>Footer</h2>
     </div>
    );
