c24e52def00ae019030c3cdd3417db377663313e
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2021-04-14 08:29:32 -0500

    Configurando para Next js

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// components
+
+//
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  )
+}
---
+++ pages/registro.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Registro = () => {
+  return ( 
+    <h1>Registrar ..</h1>
+   );
+}
+ 
+export default Registro;
