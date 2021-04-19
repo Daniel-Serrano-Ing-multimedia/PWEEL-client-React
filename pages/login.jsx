2608391ca11e24f924f811b4a908666fbc8ce48e
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2021-04-18 22:34:43 -0500

    Conectando Formularios de registro y login a API

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,16 +1,95 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// router
+import { useRouter } from 'next/router';
+//redux
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUsuario } from '../actions/authActions';
+//components
+import InputField from '../components/InputField';
 //styles
 import styles from '../styles/Forms.module.scss';
+// validaciones
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-const Login = () => {
+const Login = (  ) => {
+  // Routing
+  const router = useRouter();
+  // 
+  const authState = useSelector ( state => state.authReducer );
+  const dispatch = useDispatch();
+  // *******************************************
+  // ***************** useEffect ***************
+  // *******************************************
+  useEffect(() => {
+    console.log( 'newstate efect ', authState )
+    if( authState.isauthenticated ){ 
+        router.push( '/' );
+    }
+  }, [ authState ]);
+  // *******************************************
+  // ***************** Formik ******************
+  // *******************************************
+  const formik = useFormik({
+    initialValues :{
+      email : '',    
+      password : '',
+    },
+    validationSchema: Yup.object({
+      email : Yup.string() .email('El Email no es valido')
+      .required('El Email es Obligatorio'),
+      password : Yup.string()
+        .required('El password es obligatorio')
+    }),
+    onSubmit: async valores =>{
+      console.log(' valores ');
+      dispatch( loginUsuario( valores ) );
+    }
+  });
+
   return ( 
-    <div className = { styles.form }>
-      <div>
-        <div className = 'campo' >
-          <label>Nombre :</label>
-        </div>
+    <form
+      className = { styles.form}
+      onSubmit = { formik.handleSubmit }
+      data-cy = 'registro-form'
+    >
+    <h2  
+      data-cy = 'registro-title'
+      className = { styles.form_title }
+    >Accede</h2>
+    <div className = { styles.form_container }>          
+      <div  className = { styles.form_container_inputs }>
+        < InputField
+          inputField = 'email'
+          placeholder = 'example@example'
+          label = 'Email :'
+          type = 'email' 
+          value = { formik.values.email }
+          error = { formik.errors.email }
+          touched = { formik.touched.email }
+          handleChange = { formik.handleChange }
+          handleBlur = { formik.handleBlur }
+        />
+        < InputField
+            inputField = 'password'
+            placeholder = 'paswword'
+            label = 'PAssword :'
+            type = 'password' 
+            value = { formik.values.password }
+            error = { formik.errors.password }
+            touched = { formik.touched.password }
+            handleChange = { formik.handleChange }
+            handleBlur = { formik.handleBlur }
+          />
       </div>
+        <input 
+          data-cy = 'registro-input-submit'
+          type="submit"
+          className = { styles.form_submit }
+          value = 'Acceder'
+         />
     </div>
+  </form>
    );
 }
  
--- reducers/authReducer.js
+++ reducers/authReducer.js
@@ -1,6 +1,8 @@
 import {
   REGISTRO_EXITOSO,
   REGISTRO_ERROR,
+  LOGIN_EXITOSO,
+  LOGIN_ERREOR
 } from '../types';
 
 const initialState = {
@@ -10,21 +12,34 @@ const initialState = {
   user: null,
   token: null,
   isauthenticated: false,
+  refreshToken : null,
 }
 
 const authReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
+  const { type, payload } = action;
+  switch ( type ) {
     case REGISTRO_EXITOSO:
       return {
         ...state,
-        message : action.payload,
+        message : payload,
         error : null
       }
+    case LOGIN_ERREOR:
     case REGISTRO_ERROR:
       return {
         ...state,
-        message : action.payload.message,
-        error : action.payload.error
+        message : payload,
+        error : true
+      }
+    case LOGIN_EXITOSO:
+      localStorage.setItem( 'authToken', payload.accessToken );
+      return{
+        ...state,
+        token : payload.accessToken,
+        message: payload.message,
+        refreshToken: payload.refreshTosken,
+        isauthenticated : true,
+        error: false
       }
     default:
       return state;
